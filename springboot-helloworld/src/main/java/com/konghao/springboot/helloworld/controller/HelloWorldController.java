0cdae8621b7e25fe940c43bf7f0f000e078989c8
konghao <konghao8013@163.com>
2017-12-18 20:24:16 +0800

    增加环境变量的配置

--- springboot-helloworld/src/main/java/com/konghao/springboot/helloworld/controller/HelloWorldController.java
+++ springboot-helloworld/src/main/java/com/konghao/springboot/helloworld/controller/HelloWorldController.java
@@ -1,12 +1,16 @@
 package com.konghao.springboot.helloworld.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Value("${konghao.msg}")
+	private String msg;
 	@RequestMapping("/hello")
 	public String index() {
-		return "helljo world konghao";
+		return this.msg;
 	}
 }
