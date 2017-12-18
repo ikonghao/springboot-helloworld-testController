655c244f1013aa90c165fe8f5d510ec4d4cd43cc
konghao <konghao8013@163.com>
2017-12-18 19:11:11 +0800

    提交第一个spring boot http应用程序

---
+++ springboot-helloworld-myeclipse/src/main/java/com/konghao/demo/SpringbootHelloworldMyeclipseApplication.java
@@ -0,0 +1,12 @@
+package com.konghao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldMyeclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldMyeclipseApplication.class, args);
+	}
+}
