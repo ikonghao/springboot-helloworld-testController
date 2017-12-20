4813edf7cc099b53e44b9997203dde1106acaadd
konghao <konghao8013@163.com>
2017-12-20 19:19:11 +0800

    注解说明，和spring 自定义异常@ControllerAdvice

--- springboot-helloworld/src/main/java/com/konghao/springboot/helloworld/controller/TestException.java
+++ springboot-helloworld/src/main/java/com/konghao/springboot/helloworld/controller/TestException.java
@@ -1,23 +1,19 @@
 package com.konghao.springboot.helloworld.controller;
 
-import static org.mockito.Matchers.intThat;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.konghao.springboot.helloworld.exception.BusinessException;
-
+/**
+ * 孔浩自定义异常测试
+ * @author kongh
+ *
+ */
 @RestController
-public class HelloWorldController {
-
-	@Value("${konghao.msg}")
-	private String msg;
-
-	@RequestMapping("/hello")
+public class TestException {
+	@RequestMapping("advice")
 	public String index() {
-		//int no = 1 / 0;
-		
-		return this.msg;
+		throw new BusinessException("-120", "HelloWorldController.index 孔浩自定义的异常");
+	
 	}
 }
